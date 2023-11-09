62fa03358ba0cd3a61b0ee736d781de5c1887e2b
Faizan Rasul <faizan.rasul@meterian.io>
2023-11-09 10:57:03 +0000

    rewrote main

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ import os
 import csv
 import re
 import argparse
-import sys
 
 access_token = os.environ.get("METERIAN_ACCESS_TOKEN_QA")
 reports_api = "https://qa.meterian.com/api/v1/reports"
@@ -15,51 +14,38 @@ headers = {
     "Authorization": f"Token {access_token}"
 }
 
+name_to_timestamps = {}
 libraries_set = set()
 library_to_projects = {}
 library_to_copyright = {}
 library_to_licenses = {}
 
-def main(args=None):
+def main():
+    project_entries, csv_filename = read_through_projects()
     
-    # target_tag, csv_filename = arguments()
-    csv_filename = None
+    read_through_bibles(project_entries)
+    
+    write_bibles_to_csv(csv_filename)
+
+def read_through_projects():
+    target_tag, csv_filename = check_arguments()
+    project_entries = projects_get_request(target_tag)
+    store_latest_timestamps(project_entries)
+    
+    return project_entries, csv_filename
+
+def check_arguments():
+    target_tag, csv_filename = arguments()
+
     if csv_filename is None:
         csv_filename = "bibles"
     csv_filename += ".csv"
 
-    reports_response = requests.get(reports_api, headers=headers)
+    create_csv_file(csv_filename)
 
-    if reports_response.status_code == 200:
-        reports_data = reports_response.json()
+    return target_tag, csv_filename
 
-        # reports_data = tag_search(target_tag, reports_data)
-
-        name_to_timestamps = {}
-        store_latest_timestamps(reports_data, name_to_timestamps)
-
-        create_csv_file(csv_filename)
-
-        for report in reports_data:
-            report_uuid = report["uuid"]
-            report_name = report.get("name")
-            report_branch = report.get("branch")
-            report_timestamp = report.get("timestamp")
-
-            if report_timestamp != name_to_timestamps[report_name]:
-                continue
-
-            bible_get_request(report_uuid, report_name, report_branch)
-
-            for library in libraries_set:
-                with open(csv_filename, mode='a', newline='') as csv_file:
-                    writer = csv.writer(csv_file)
-                    writer.writerow([library_to_licenses[library], library, library_to_copyright[library], library_to_projects[library]])
-
-    else:
-        print(f"Error: {reports_response.status_code}")
-
-def arguments(args=None):
+def arguments():
     parser = argparse.ArgumentParser(description='generates csv file of bibles from the meterian api')
     parser.add_argument('-t', '--tag', type=str, help='filter projects by tag')
     parser.add_argument('-n', '--name', type=str, help='name the csv file')
@@ -68,14 +54,6 @@ def arguments(args=None):
     target_tag = args.tag
     csv_filename = args.name
     return target_tag, csv_filename
-
-def tag_search(target_tag, data):
-    if target_tag is not None:
-        if target_tag:
-            data = [item for item in data if re.search(target_tag, str(item.get("tags")))]
-            if not data:
-                print(f"No items with the specified tag '{target_tag}' found.")
-    return data
 
 def create_csv_file(csv_filename):
     csv_headers = [
@@ -86,7 +64,27 @@ def create_csv_file(csv_filename):
         writer = csv.writer(csv_file)
         writer.writerow(csv_headers)
 
-def store_latest_timestamps(reports_data, name_to_timestamps):
+def projects_get_request(target_tag):
+    reports_response = requests.get(reports_api, headers=headers)
+
+    if reports_response.status_code == 200:
+        reports_data = reports_response.json()
+        
+        reports_data = tag_search(target_tag, reports_data)
+        
+        return reports_data
+    else:
+        print(f"Error: {reports_response.status_code}")
+        
+def tag_search(target_tag, data):
+    if target_tag is not None:
+        if target_tag:
+            data = [item for item in data if re.search(target_tag, str(item.get("tags")))]
+            if not data:
+                print(f"No items with the specified tag '{target_tag}' found.")
+    return data
+
+def store_latest_timestamps(reports_data):
     for report in reports_data:
         report_name = report.get("name")
         report_timestamp = report.get("timestamp")
@@ -97,7 +95,19 @@ def store_latest_timestamps(reports_data, name_to_timestamps):
 
         else:
             name_to_timestamps[report_name] = report_timestamp
-
+            
+def read_through_bibles(project_entries):
+    for project_entry in project_entries:
+        project_uuid = project_entry["uuid"]
+        project_name = project_entry.get("name")
+        project_branch = project_entry.get("branch")
+        project_timestamp = project_entry.get("timestamp")
+        
+        if project_timestamp != name_to_timestamps[project_name]:
+            continue
+        
+        bible_get_request(project_uuid, project_name, project_branch)
+        
 def bible_get_request(report_uuid, report_name, report_branch):
     project_url = f"https://qa.meterian.com/api/v1/reports/{report_uuid}/bible"
     percentage = bible_post_request(report_uuid, report_name, report_branch, project_url)
@@ -110,26 +120,7 @@ def bible_get_request(report_uuid, report_name, report_branch):
             bible_json_to_map(bible_data, report_name, report_branch)
         else:
             print(f"Error, trying to get bible for {report_name}:{report_branch}: {bible_get_response.status_code}")
-
-def bible_json_to_map(bible_data, project_name, project_branch):
-    project = f"{project_name}:{project_branch}"
-
-    for language, array in bible_data['components'].items():
-        for item in array:
-            library = f"{language}:{item['name']}:{item['version']}"
-            if library not in libraries_set:
-                libraries_set.add(library)
-                library_to_projects[library] = project
-            else:
-                library_to_projects[library] += f";{project}"
-
-            copyright = item.get('copyright', {}).get('text', '').replace('\n', '') if 'copyright' in item else ''
-
-            licenses = ', '.join(item.get('licenses', []))
-
-            library_to_copyright[library] = f"{copyright}"
-            library_to_licenses[library] = licenses
-
+            
 def bible_post_request(project_uuid, project_name, project_branch, project_url):
     project_post_response = requests.post(project_url, headers=headers, data="")
 
@@ -142,7 +133,7 @@ def bible_post_request(project_uuid, project_name, project_branch, project_url):
             return percentage
     else:
         print(f"Error, trying to get iden for {project_name}:{project_branch}: {project_post_response.status_code}")
-
+        
 def check_percentage(id_api, headers, project_name, project_branch):
     current_percentage = 0
     percentage_not_changed_count = 0
@@ -169,6 +160,31 @@ def check_percentage(id_api, headers, project_name, project_branch):
             print(f"Error, trying to get percentage for {project_name}: {id_response.status_code}")
 
         time.sleep(polling_interval)
+        
+def bible_json_to_map(bible_data, project_name, project_branch):
+    project = f"{project_name}:{project_branch}"
 
+    for language, array in bible_data['components'].items():
+        for item in array:
+            library = f"{language}:{item['name']}:{item['version']}"
+            if library not in libraries_set:
+                libraries_set.add(library)
+                library_to_projects[library] = project
+            else:
+                library_to_projects[library] += f";{project}"
+
+            copyright = item.get('copyright', {}).get('text', '').replace('\n', '') if 'copyright' in item else ''
+
+            licenses = ', '.join(item.get('licenses', []))
+
+            library_to_copyright[library] = f"{copyright}"
+            library_to_licenses[library] = licenses
+            
+def write_bibles_to_csv(csv_filename):
+    for library in libraries_set:
+        with open(csv_filename, mode='a', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([library_to_licenses[library], library, library_to_copyright[library], library_to_projects[library]])
+            
 if __name__ == "__main__":
     main()
